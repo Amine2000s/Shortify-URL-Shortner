8a58591df8585c9e9dba5bd27a838000211acfec
Chabi Amine sif eddine <chabiaminesifeddine@gmail.com>
2025-07-04 15:52:40 +0100

    fixed security issue

--- src/main/java/com/chabiamine/Shortify/config/SecurityConfig.java
+++ src/main/java/com/chabiamine/Shortify/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/Home","/signup","/css/**","/js/**").permitAll()
+                        .requestMatchers("/Home","/signup","/css/**","/js/**","/Shortify/**").permitAll()
                 .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
